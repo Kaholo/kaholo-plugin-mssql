a32335aa3773b459b9403a33d8bbb2e5f8eb9bdb
Kenny Marsh <kenny.m@kaholo.io>
2023-01-30 12:29:48 +0100

    eslinted

--- mssql.service.js
+++ mssql.service.js
@@ -1,105 +1,145 @@
-const sql = require('mssql')
-const fs = require('fs');
+const sql = require("mssql");
+const fs = require("fs");
 
-module.exports = class MSSQLService{
-    constructor({conStr}){
-        if (!conStr) throw "Must provide a connection string to use to connect to MSSQL.";
-        this.conStr = conStr;
-        this.rolledback = false;
+module.exports = class MSSQLService {
+  constructor({ conStr }) {
+    if (!conStr) {
+      throw new Error("Must provide a connection string to use to connect to MSSQL.");
     }
+    this.conStr = conStr;
+    this.rolledback = false;
+  }
 
-    static translatePermissionScope(scope){
-        return  scope == "readWrite" ? "INSERT, DELETE, UPDATE, SELECT" : 
-                scope == "write" ? "INSERT, DELETE, UPDATE" : 
-                scope == "read" ? "SELECT" : 
-                scope == "full" ? "ALL" : 
-                scope;
+  static translatePermissionScope(scope) {
+    switch (scope) {
+      case "readWrite":
+        return "INSERT, DELETE, UPDATE, SELECT";
+      case "write":
+        return "INSERT, DELETE, UPDATE";
+      case "read":
+        return "SELECT";
+      case "full":
+        return "ALL";
+      default:
+        // do nothing
     }
+    return scope;
+  }
 
-    static async from(params, settings){
-        const result = new MSSQLService({
-            conStr: params.conStr || settings.conStr
-        });
-        await result.connect();
-        return result;
-    }
+  static async from(params, settings) {
+    const result = new MSSQLService({
+      conStr: params.conStr || settings.conStr,
+    });
+    await result.connect();
+    return result;
+  }
 
-    async connect(){
-        this.pool = await sql.connect(this.conStr);
-    }
+  async connect() {
+    this.pool = await sql.connect(this.conStr);
+  }
 
-    async startTransaction(){
-        this.transaction = await new sql.Transaction(this.pool);
-        this.transaction.on('rollback', () => {this.rolledback = true});
-        await this.transaction.begin();
-    }
-    
-    async rollback(){
-        if (!this.rolledback) await this.transaction.rollback();
-    }
+  async startTransaction() {
+    this.transaction = await new sql.Transaction(this.pool);
+    this.transaction.on("rollback", () => {
+      this.rolledback = true;
+    });
+    await this.transaction.begin();
+  }
 
-    async commitTransaction(){
-        await this.transaction.commit();
+  async rollback() {
+    if (!this.rolledback) {
+      await this.transaction.rollback();
     }
-    
-    async close(){
-        await this.pool.close();
-    }
+  }
 
-    async executeQuery({query, dontClose, db, getRecordset}){
-        if (!query) throw "Must specify SQL query to execute";
-        if (db && db !== "*" && db !== "dbo") query += `USE ${db};\n`;
-        const request = this.transaction ? new sql.Request(this.transaction) : this.pool.request();
-        const result = await request.query(query);
-        if (!dontClose){
-            await this.close();
-        }
-        return getRecordset ? result.recordset : result;
+  async commitTransaction() {
+    await this.transaction.commit();
+  }
+
+  async close() {
+    await this.pool.close();
+  }
+
+  async executeQuery({
+    query, dontClose, db, getRecordset,
+  }) {
+    if (!query) {
+      throw new Error("Must specify SQL query to execute");
     }
-    
-    async executeSQLFile({path}){
-        if (!path) throw "Must specify path of SQL script to execute";
-        if (!fs.existsSync(path)) throw `File '${path}' wasn't found`;
-        const sqlScript = fs.readFileSync(path, 'utf-8');
-        return this.executeQuery({query: sqlScript})
+    let revisedQuery = query;
+    if (db && db !== "*" && db !== "dbo") {
+      revisedQuery += `USE ${db};\n`;
     }
-    
-    async testConnectivity(){
-        await this.close();
-        return "Connected";
+    const request = this.transaction ? new sql.Request(this.transaction) : this.pool.request();
+    const result = await request.query(revisedQuery);
+    if (!dontClose) {
+      await this.close();
     }
-    
-    async getTablesLocks({db, table}){
-        if (db === "*") db = undefined;
-        if (table === "*") table = undefined;
-        const query = `SELECT * FROM sys.dm_tran_locks${db ? `
-WHERE resource_database_id = DB_ID()`: ""}${table ? `
-AND resource_associated_entity_id = OBJECT_ID(N'${db}.${table}')` : ""};`
-        return this.executeQuery({query, db, getRecordset: true});
+    return getRecordset ? result.recordset : result;
+  }
+
+  async executeSQLFile({ path }) {
+    if (!path) {
+      throw new Error("Must specify path of SQL script to execute");
     }
-    
-    async getServerVersion(){
-        const query = `SELECT @@version;`;
-        return this.executeQuery({query, getRecordset: true});
+    if (!fs.existsSync(path)) {
+      throw new Error(`File '${path}' wasn't found`);
     }
-    
-    async getDbSize({db}){
-        if (db === "*") db = undefined;
-        const query = `SELECT 
+    const sqlScript = fs.readFileSync(path, "utf-8");
+    return this.executeQuery({ query: sqlScript });
+  }
+
+  async testConnectivity() {
+    await this.close();
+    return "Connected";
+  }
+
+  async getTablesLocks({ db, table }) {
+    let targetDb = db;
+    let targetTable = table;
+    if (targetDb === "*") {
+      targetDb = undefined;
+    }
+    if (targetTable === "*") {
+      targetTable = undefined;
+    }
+    const query = `SELECT * FROM sys.dm_tran_locks${targetDb ? `
+WHERE resource_database_id = DB_ID()` : ""}${targetTable ? `
+AND resource_associated_entity_id = OBJECT_ID(N'${targetDb}.${targetTable}')` : ""};`;
+    return this.executeQuery({ query, targetDb, getRecordset: true });
+  }
+
+  async getServerVersion() {
+    const query = "SELECT @@version;";
+    return this.executeQuery({ query, getRecordset: true });
+  }
+
+  async getDbSize({ db }) {
+    let targetDb = db;
+    if (targetDb === "*") {
+      targetDb = undefined;
+    }
+    const query = `SELECT 
     database_name = DB_NAME(database_id), 
     log_size_mb = CAST(SUM(CASE WHEN type_desc = 'LOG' THEN size END) * 8. / 1024 AS DECIMAL(8,2)), 
     row_size_mb = CAST(SUM(CASE WHEN type_desc = 'ROWS' THEN size END) * 8. / 1024 AS DECIMAL(8,2)), 
     total_size_mb = CAST(SUM(size) * 8. / 1024 AS DECIMAL(8,2))
-FROM sys.master_files WITH(NOWAIT)${(db) ? `
+FROM sys.master_files WITH(NOWAIT)${(targetDb) ? `
 WHERE database_id = DB_ID()` : ""}
 GROUP BY database_id;`;
-        return this.executeQuery({query, db, getRecordset: true});
+    return this.executeQuery({ query, targetDb, getRecordset: true });
+  }
+
+  async getTablesSize({ db, table }) {
+    let targetDb = db;
+    let targetTable = table;
+    if (targetDb === "*") {
+      targetDb = undefined;
     }
-    
-    async getTablesSize({db, table}){
-        if (db === "*") db = undefined;
-        if (table === "*") table = undefined;
-        const query = `SELECT 
+    if (targetTable === "*") {
+      targetTable = undefined;
+    }
+    const query = `SELECT 
     t.NAME AS table_name,
     s.Name AS database_name,
     p.rows,
@@ -116,153 +156,176 @@ INNER JOIN
     sys.allocation_units a ON p.partition_id = a.container_id
 LEFT OUTER JOIN 
     sys.schemas s ON t.schema_id = s.schema_id
-WHERE${db ? `
-    s.Name = '${db}' AND` : ""}
-    t.NAME ${table ? `= '${table}'` : "NOT LIKE 'dt%'"} 
+WHERE${targetDb ? `
+    s.Name = '${targetDb}' AND` : ""}
+    t.NAME ${targetTable ? `= '${targetTable}'` : "NOT LIKE 'dt%'"} 
     AND t.is_ms_shipped = 0
     AND i.OBJECT_ID > 255 
 GROUP BY 
     t.Name, s.Name, p.Rows
 ORDER BY 
     total_space_mb DESC, t.Name, s.Name;`;
-    return this.executeQuery({query, getRecordset: true});
+    return this.executeQuery({ query, getRecordset: true });
+  }
+
+  async createUser({
+    user, pass, role, db, table, dbScope, tableScope,
+  }) {
+    if (!user || !pass) {
+      throw new Error("Didn't provide one of the required parameters.");
     }
-    
-    async createUser({user, pass, role, db, table, dbScope, tableScope}){
-        if (!user || !pass) throw "Didn't provide one of the required parameters.";
-        const query = `CREATE LOGIN ${user} WITH PASSWORD = '${pass}';
+    const query = `CREATE LOGIN ${user} WITH PASSWORD = '${pass}';
 CREATE USER ${user} FOR LOGIN ${user};`;
-        await this.startTransaction();
-        let lastAction = "createUser";
-        try {
-            const result = {createUser: await this.executeQuery({query, dontClose: true})};
-            if (role){
-                lastAction = "addRole";
-                result.addRole = await this.addRoleMember({user, role, dontClose: true});
-            }
-            if (dbScope){
-                lastAction = "grantDbPermissions";
-                result.grantDbPermissions = await this.grantDbPermissions({
-                    user, db, 
-                    scope: dbScope, 
-                    dontClose: true
-                });
-            }
-            if (tableScope){
-                lastAction = "grantTablePermissions";
-                result.grantTablePermissions = await this.grantTablePermissions({
-                    user, db, table, 
-                    scope: tableScope, 
-                    dontClose: true
-                });
-            }
-            lastAction = "commitTransaction";
-            await this.commitTransaction();
-            return result;
-        }
-        catch (error) {
-            await this.rollback();
-            throw `Error with '${lastAction}': ${error.message || JSON.stringify(error)}`;
-        }
-        finally {
-            await this.close();
-        }
+    await this.startTransaction();
+    let lastAction = "createUser";
+    try {
+      const result = { createUser: await this.executeQuery({ query, dontClose: true }) };
+      if (role) {
+        lastAction = "addRole";
+        result.addRole = await this.addRoleMember({ user, role, dontClose: true });
+      }
+      if (dbScope) {
+        lastAction = "grantDbPermissions";
+        result.grantDbPermissions = await this.grantDbPermissions({
+          user,
+          db,
+          scope: dbScope,
+          dontClose: true,
+        });
+      }
+      if (tableScope) {
+        lastAction = "grantTablePermissions";
+        result.grantTablePermissions = await this.grantTablePermissions({
+          user,
+          db,
+          table,
+          scope: tableScope,
+          dontClose: true,
+        });
+      }
+      lastAction = "commitTransaction";
+      await this.commitTransaction();
+      return result;
+    } catch (error) {
+      await this.rollback();
+      throw new Error(`Error with '${lastAction}': ${error.message || JSON.stringify(error)}`);
+    } finally {
+      await this.close();
+    }
+  }
+
+  async addRoleMember({ user, role, dontClose }) {
+    if (!user || !role) {
+      throw new Error("Didn't provide one of the required parameters.");
+    }
+    const query = `EXEC sp_addrolemember '${role}', '${user}';`;
+    return this.executeQuery({ query, dontClose });
+  }
+
+  async grantDbPermissions({
+    user, db, scope, dontClose,
+  }) {
+    if (!scope || !user || !db) {
+      throw new Error("Didn't provide one of the required parameters.");
     }
 
-    async addRoleMember({user, role, dontClose}){
-        if (!user || !role) throw "Didn't provide one of the required parameters.";
-        const query = `EXEC sp_addrolemember '${role}', '${user}';`;
-        return this.executeQuery({query, dontClose});
+    const query = `GRANT ${MSSQLService.translatePermissionScope(scope)} TO ${user};`;
+
+    return this.executeQuery({ query, db, dontClose });
+  }
+
+  async grantTablePermissions({
+    user, db, table, scope, dontClose,
+  }) {
+    if (!scope || !user || !db || !table) {
+      throw new Error("Didn't provide one of the required parameters.");
     }
 
-    async grantDbPermissions({user, db, scope, dontClose}){
-        if (!scope || !user || !db) throw "Didn't provide one of the required parameters.";
+    const query = `GRANT ${MSSQLService.translatePermissionScope(scope)} ON ${db}.${table} TO ${user};`;
 
-        const query = `GRANT ${MSSQLService.translatePermissionScope(scope)} TO ${user};`;
-        
-        return this.executeQuery({query, db, dontClose});
-    }
-    
-    async grantTablePermissions({user, db, table, scope, dontClose}){
-        if (!scope || !user || !db || !table) throw "Didn't provide one of the required parameters.";
-        
-        const query = `GRANT ${MSSQLService.translatePermissionScope(scope)} ON ${db}.${table} TO ${user};`;
-        
-        return this.executeQuery({query, db, dontClose});
-    }
+    return this.executeQuery({ query, db, dontClose });
+  }
 
-    async createRole({role, db, table, dbScope, tableScope}){
-        if (!role) throw "Must specify role to create";
-        const query = `CREATE ROLE ${role};`;
-        await this.startTransaction();
-        let lastAction = "createRole";
-        try {
-            const result = {createRole: await this.executeQuery({
-                dontClose: true,
-                query
-            })};
-            if (dbScope){
-                lastAction = "grantDbPermissions";
-                result.grantDbPermissions = await this.grantDbPermissions({
-                    user: role, 
-                    scope: dbScope, 
-                    dontClose: true,
-                    db
-                });
-            }
-            if (tableScope){
-                lastAction = "grantTablePermissions";
-                result.grantTablePermissions = await this.grantTablePermissions({
-                    user: role, 
-                    scope: tableScope, 
-                    dontClose: true,
-                    db, table
-                });
-            }
-            lastAction = "commitTransaction";
-            await this.commitTransaction();
-            return result;
-        }
-        catch (error) {
-            await this.rollback();
-            throw `Error with '${lastAction}': ${error.message || JSON.stringify(error)}`;
-        }
-        finally {
-            await this.close();
-        }
+  async createRole({
+    role, db, table, dbScope, tableScope,
+  }) {
+    if (!role) {
+      throw new Error("Must specify role to create");
     }
-    
-    async listDbs(){
-        const query = `SELECT name FROM sys.databases;`;
-        const result = [{name: "dbo"}, ...(await this.executeQuery({query, getRecordset: true}))]
-        return result;
+    const query = `CREATE ROLE ${role};`;
+    await this.startTransaction();
+    let lastAction = "createRole";
+    try {
+      const result = {
+        createRole: await this.executeQuery({
+          dontClose: true,
+          query,
+        }),
+      };
+      if (dbScope) {
+        lastAction = "grantDbPermissions";
+        result.grantDbPermissions = await this.grantDbPermissions({
+          user: role,
+          scope: dbScope,
+          dontClose: true,
+          db,
+        });
+      }
+      if (tableScope) {
+        lastAction = "grantTablePermissions";
+        result.grantTablePermissions = await this.grantTablePermissions({
+          user: role,
+          scope: tableScope,
+          dontClose: true,
+          db,
+          table,
+        });
+      }
+      lastAction = "commitTransaction";
+      await this.commitTransaction();
+      return result;
+    } catch (error) {
+      await this.rollback();
+      throw new Error(`Error with '${lastAction}': ${error.message || JSON.stringify(error)}`);
+    } finally {
+      await this.close();
     }
-    
-    async listRoles(){
-        const query = `SELECT name FROM sysusers WHERE issqlrole = 1;`;
-        return this.executeQuery({query, getRecordset: true});
+  }
+
+  async listDbs() {
+    const query = "SELECT name FROM sys.databases;";
+    const result = [{ name: "dbo" }, ...(await this.executeQuery({ query, getRecordset: true }))];
+    return result;
+  }
+
+  async listRoles() {
+    const query = "SELECT name FROM sysusers WHERE issqlrole = 1;";
+    return this.executeQuery({ query, getRecordset: true });
+  }
+
+  async listUsers() {
+    const query = "SELECT name FROM sysusers;";
+    return this.executeQuery({ query, getRecordset: true });
+  }
+
+  async listTables({ db }) {
+    let targetDb = db;
+    if (targetDb === "*") {
+      targetDb = undefined;
     }
-    
-    async listUsers(){
-        const query = `SELECT name FROM sysusers;`;
-        return this.executeQuery({query, getRecordset: true});
-    }
-    
-    async listTables({db}){
-        if (db === "*") db = undefined;
-        const query = `SELECT 
+    const query = `SELECT 
         t.NAME AS name,
         s.Name AS database_name
     FROM 
         sys.tables t
     LEFT OUTER JOIN 
-        sys.schemas s ON t.schema_id = s.schema_id${db ? `
+        sys.schemas s ON t.schema_id = s.schema_id${targetDb ? `
     WHERE
-        s.Name = '${db}'` : ""}
+        s.Name = '${targetDb}'` : ""}
     GROUP BY
         t.Name, s.Name
     ORDER BY
         t.Name, s.Name;`;
-        return this.executeQuery({query, getRecordset: true});
-    }
-}
+    return this.executeQuery({ query, getRecordset: true });
+  }
+};
--- parsers.js
+++ parsers.js
@@ -1,76 +1,115 @@
-const os = require('os');
-const homeDirectory = os.homedir();
-const isWin = os.platform()=='win32';
-const normalize = require('path').normalize;
+const os = require("os");
 
-function untildify(path){
-    return homeDirectory ? path.replace(/^~(?=$|\/|\\)/, homeDirectory) : path;
+const homeDirectory = os.homedir();
+const isWin = os.platform() === "win32";
+const { normalize } = require("path");
+
+function untildify(path) {
+  return homeDirectory ? path.replace(/^~(?=$|\/|\\)/, homeDirectory) : path;
 }
 
-function parseArray(value){
-    if (!value) return [];
-    if (Array.isArray(value)) return value;
-    if (typeof(value) === "string") return value.split("\n").map(line=>line.trim()).filter(line=>line);
-    throw "Unsupprted array format";
+function parseArray(value) {
+  if (!value) {
+    return [];
+  }
+  if (Array.isArray(value)) {
+    return value;
+  }
+  if (typeof (value) === "string") {
+    return value.split("\n").map((line) => line.trim()).filter((line) => line);
+  }
+  throw new Error("Unsupprted array format");
 }
 
 module.exports = {
-    boolean : (value) =>{
-        if (!value || value === "false") return false;
-        return true;
-    },
-    text : (value) =>{
-        if (value)
-            return value.split('\n');
-        return undefined;
-    },
-    number: (value)=>{
-        if (!value) return undefined;
-        const parsed = parseInt(value);
-        if (parsed === NaN) {
-            throw `Value ${value} is not a valid number`
+  boolean: (value) => {
+    if (!value || value === "false") {
+      return false;
+    }
+    return true;
+  },
+  text: (value) => {
+    if (value) {
+      return value.split("\n");
+    }
+    return undefined;
+  },
+  number: (value) => {
+    if (!value) {
+      return undefined;
+    }
+    const parsed = parseInt(value, 10);
+    if (Number.isNaN(parsed)) {
+      throw new Error(`Value ${value} is not a valid number`);
+    }
+    return parsed;
+  },
+  autocomplete: (value, getVal) => {
+    if (!value) {
+      return undefined;
+    }
+    if (typeof (value) === "object") {
+      return (getVal ? value.value : value.id) || value;
+    }
+    return value;
+  },
+  autocompleteOrArray: (value) => {
+    if (!value) {
+      return [];
+    }
+    if (Array.isArray(value)) {
+      return value;
+    }
+    if (typeof (value) === "object") {
+      return [value.id || value];
+    }
+    return [value];
+  },
+  object: (value) => {
+    if (!value) {
+      return undefined;
+    }
+    if (typeof (value) === "object") {
+      return value;
+    }
+    if (typeof (value) === "string") {
+      const obj = value.split("\n").map((line) => {
+        let [key, ...lineValues] = line.split("=");
+        if (!lineValues.length) {
+          throw new Error("Unsupported object format");
         }
-        return parsed;
-    },
-    autocomplete: (value, getVal)=>{
-        if (!value) return undefined;
-        if (typeof(value) == "object") return (getVal ? value.value : value.id) || value;
-        return value;
-    },
-    autocompleteOrArray: (value)=>{
-        if (!value) return [];
-        if (Array.isArray(value)) return value;
-        if (typeof(value) == "object") return [value.id || value];
-        return [value];
-    },
-    object: (value)=>{
-        if (!value) return undefined;
-        if (typeof(value) === "object") return value;
-        if (typeof(value) === "string") {
-            const obj = {};
-            value.split("\n").map(line=>{
-                var [key, ...value] = line.split("=");
-                if (!value.length) throw "Unsupported object format";
-                key = key.trim();
-                value = value.join("=").trim();
-                if (!key || !value) throw "Empty key or value in string:\n"+value;
-                obj[key] = value;
-            })
-            return obj;
+        key = key.trim();
+        lineValues = lineValues.join("=").trim();
+        if (!key || !lineValues) {
+          throw new Error(`Empty key or value in string:\n${value}`);
         }
-        throw `Value ${value} is not an object`;
-    },
-    string: (value)=>{
-        if (!value) return undefined;
-        if (typeof(value) === "string") return value.trim();
-        throw `Value ${value} is not a valid string`;
-    },
-    path: (value)=>{
-        if (!value) return undefined;
-        let path = module.exports.string(value);
-        if (isWin) path = path.replace(/\//g, '\\');
-        else path = path.replace(/\\/g, '/');
-        return normalize(untildify(path));
-    },
-    array: parseArray
-}
+        obj[key] = lineValues;
+        return obj;
+      });
+      return obj;
+    }
+    throw new Error(`Value ${value} is not an object`);
+  },
+  string: (value) => {
+    if (!value) {
+      return undefined;
+    }
+    if (typeof (value) === "string") {
+      return value.trim();
+    }
+    throw new Error(`Value ${value} is not a valid string`);
+  },
+  path: (value) => {
+    if (!value) {
+      return undefined;
+    }
+    let path = module.exports.string(value);
+    if (isWin) {
+      path = path.replace(/\//g, "\\");
+    } else {
+      path = path.replace(/\\/g, "/");
+    }
+    return normalize(untildify(path));
+  },
+  array: parseArray,
+};
